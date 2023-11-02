5d5a08e95c6940dbfce1051f5a0d4a864da2676b
Turadg Aleahmad <turadg@agoric.com>
2023-11-02 14:06:07 -0700

    feat: name match for CLIs

--- buildTestImages.ts
+++ buildTestImages.ts
@@ -1,10 +1,24 @@
 #!/usr/bin/env tsx
 // @ts-check
 
-import { execSync } from 'child_process';
+import { parseArgs } from 'node:util';
+import { execSync } from 'node:child_process';
 import { imageNameForProposalTest, readProposals } from './common';
 
-for (const proposal of readProposals()) {
+const options = {
+  match: { short: 'm', type: 'string' },
+};
+const { values } = parseArgs({ options });
+
+const { match } = values;
+
+const allProposals = readProposals();
+
+const proposals = match
+  ? allProposals.filter(p => p.proposalName.includes(match))
+  : allProposals;
+
+for (const proposal of proposals) {
   console.log(`\nBuilding test image for proposal ${proposal.proposalName}`);
   const { name, target } = imageNameForProposalTest(proposal);
   const cmd = `docker build --tag ${name} --target ${target} .`;
