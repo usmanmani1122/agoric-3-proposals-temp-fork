3b6ebce0efa006a602664bfb06ffbf761bd07314
Dan Connolly <connolly@agoric.com>
2023-11-07 17:00:47 -0600

    chore: prune obsolete yarn installs
    Co-authored-by: Turadg Aleahmad <turadg@agoric.com>

--- proposals/53:kread-start/eval.sh
+++ proposals/53:kread-start/eval.sh
@@ -7,14 +7,6 @@ source /usr/src/upgrade-test-scripts/env_setup.sh
 
 ls -al
 
-yarn install --frozen-lockfile
-
-# XXX to avoid rebuilding lower layers
-# TODO clean up JS lib code so it's more independent
-cp package.json yarn.lock /usr/src/upgrade-test-scripts/lib/
-cd /usr/src/upgrade-test-scripts/lib/ && yarn install --frozen-lockfile
-cd -
-
 # XXX using Ava serial to script the core-eval
 yarn ava mn2-start.test.js
 
