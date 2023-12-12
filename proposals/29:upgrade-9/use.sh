8a220b42404e4d8e51e827afe93abc44b594a0cd
Turadg Aleahmad <turadg@agoric.com>
2023-12-12 15:14:02 -0800

    test: consolidate upgrade-9 tests

--- proposals/29:upgrade-9/use.sh
+++ proposals/29:upgrade-9/use.sh
@@ -15,6 +15,3 @@ sed --in-place=.bak s/'minimum-gas-prices = ""'/'minimum-gas-prices = "0ubld,0ui
 echo "Dumping PSM gov params..."
 timeout 3 agoric follow -l :published.psm.${PSM_PAIR}.metrics -o jsonlines | tee /root/.agoric/psm_metrics.json
 timeout 3 agoric follow -l :published.psm.${PSM_PAIR}.governance -o jsonlines | tee /root/.agoric/psm_governance.json
-
-test_not_val "$(cat /root/.agoric/psm_metrics.json | wc -l)" "0" "psm metrics shouldnt be empty"
-test_not_val "$(cat /root/.agoric/psm_governance.json | wc -l)" "0" "psm gov params shouldnt be empty"
