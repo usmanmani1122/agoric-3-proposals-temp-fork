8fe84e56e791b97248cfb30f36d93d0d99341833
Turadg Aleahmad <turadg@agoric.com>
2023-12-12 15:13:33 -0800

    feat: set minimum-gas-prices explicitly to zero

--- proposals/29:upgrade-9/use.sh
+++ proposals/29:upgrade-9/use.sh
@@ -5,6 +5,9 @@ set -e
 
 source /usr/src/upgrade-test-scripts/env_setup.sh
 
+# Set to zero so tests don't have to pay gas (we're not testing that)
+sed --in-place=.bak s/'minimum-gas-prices = ""'/'minimum-gas-prices = "0ubld,0uist"'/ ~/.agoric/config/app.toml
+
 # NOTE: agoric follow doesn't have the `--first-value-only` parameter in this version
 # so we use a hack
 
