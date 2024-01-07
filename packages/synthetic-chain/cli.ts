329372a0f2bd61c8846aab306b0449ddd5f5cfd4
Turadg Aleahmad <turadg@agoric.com>
2024-01-06 16:43:37 -0800

    feat: 'amend' command to propose atop an existing image

--- packages/synthetic-chain/cli.ts
+++ packages/synthetic-chain/cli.ts
@@ -4,9 +4,12 @@ import { parseArgs } from 'node:util';
 import path from 'node:path';
 import { execSync } from 'node:child_process';
 import { buildProposalSubmissions, buildTestImages } from './src/cli/build.js';
-import { refreshDockerfile } from './src/cli/dockerfileGen.js';
+import { writeDockerfile } from './src/cli/dockerfileGen.js';
 import { matchOneProposal, readProposals } from './src/cli/proposals.js';
 import { debugTestImage, runTestImages } from './src/cli/run.js';
+
+// TODO change the tag to 'main' after multi-platform support https://github.com/Agoric/agoric-3-proposals/pull/32
+const baseImage = 'pr-32-linux_arm64_v8';
 
 const { positionals, values } = parseArgs({
   options: {
@@ -28,20 +31,31 @@ const [cmd] = positionals;
 
 // TODO consider a lib like Commander for auto-gen help
 const usage = `USAGE:
-build
+amend [<tag>]   - build on top of an existing image (defaults to latest from a3p)
 
-test [--debug]
+build           - build from the beginning
+
+test [--debug]  - run the tests of the proposals
 `;
 
+const buildImages = () => {
+  execSync(
+    // XXX very brittle
+    'cp -r node_modules/@agoric/synthetic-chain/upgrade-test-scripts .',
+  );
+  buildProposalSubmissions(proposals);
+  buildTestImages(proposals, values.dry);
+};
+
 switch (cmd) {
+  case 'amend':
+    const fromTag = positionals[1] || baseImage;
+    writeDockerfile(allProposals, fromTag);
+    buildImages();
+    break;
   case 'build':
-    execSync(
-      // XXX very brittle
-      'cp -r node_modules/@agoric/synthetic-chain/upgrade-test-scripts .',
-    );
-    refreshDockerfile(allProposals);
-    buildProposalSubmissions(proposals);
-    buildTestImages(proposals, values.dry);
+    writeDockerfile(allProposals);
+    buildImages();
     break;
   case 'test':
     if (values.debug) {
