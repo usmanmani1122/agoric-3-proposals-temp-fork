0e5eab489647574135d74a637ca3adb6ec76b126
Mathieu Hofman <mathieu@agoric.com>
2024-02-21 16:05:49 +0000

    test(zoe1): remove manual zoe core-eval

--- proposals/b:zoe1/post.test.js
+++
@@ -1,7 +0,0 @@
-import test from 'ava';
-import { getIncarnation } from '@agoric/synthetic-chain';
-
-test(`Zoe vat was upgraded`, async t => {
-  const incarantion = await getIncarnation('zoe');
-  t.is(incarantion, 1);
-});
