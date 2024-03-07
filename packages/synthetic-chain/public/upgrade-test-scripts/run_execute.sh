1b7e369e6f261dbc0c99876243ef50a42832f4b1
Mathieu Hofman <mathieu@agoric.com>
2024-03-07 23:29:52 +0000

    feat: verify upgrade has been executed

--- packages/synthetic-chain/public/upgrade-test-scripts/run_execute.sh
+++ packages/synthetic-chain/public/upgrade-test-scripts/run_execute.sh
@@ -16,6 +16,15 @@ echo "Execute the upgrade in consensus"
 
 source ./env_setup.sh
 
+PLAN_NAME=$1
+if [ -z "$PLAN_NAME" ]; then
+  fail "Must specify the plan name of the upgrade"
+fi
+
+FOUND_PLAN_NAME="$(jq -r .name $HOME/.agoric/data/upgrade-info.json)"
+
+[ "$PLAN_NAME" = "$FOUND_PLAN_NAME" ] || fail "Upgrade plan name $FOUND_PLAN_NAME does not match the expected value $PLAN_NAME" 
+
 startAgd
 
 echo "Run several blocks for the upgrade to settle, then exit"
