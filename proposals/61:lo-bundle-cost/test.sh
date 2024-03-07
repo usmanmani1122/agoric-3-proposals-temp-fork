59a4ec526a66e8d02ff90e3b3f52017ddcdb32ba
Turadg Aleahmad <turadg@agoric.com>
2024-03-07 09:59:50 -0800

    feat: print bundle cost in whole units

--- proposals/61:lo-bundle-cost/test.sh
+++ proposals/61:lo-bundle-cost/test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-expected=20000000
+expected=20
 
 params="$(agd query swingset params -o json)"
-cost="$(echo $params | jq -r ".beans_per_unit | .[] | select(.key == \"storageByte\") | .beans" )"
+ucost="$(echo $params | jq -r ".beans_per_unit | .[] | select(.key == \"storageByte\") | .beans")"
+cost=$((ucost / 1000000))
 
 # fail if cost is not expected
 if [ "$cost" != "$expected" ]; then
@@ -13,4 +14,3 @@ if [ "$cost" != "$expected" ]; then
 else
     echo "Cost is $cost"
 fi
-
