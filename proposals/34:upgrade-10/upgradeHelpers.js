f860aae2125e3567cc13c7fa081f77ac7b0fcd6c
Luqi Pan <luqi@agoric.com>
2024-03-27 09:02:58 -0700

    chore: remove print keys in JS

--- proposals/34:upgrade-10/upgradeHelpers.js
+++ proposals/34:upgrade-10/upgradeHelpers.js
@@ -1,46 +1,6 @@
-import { $ } from 'execa';
-
-import {
-  GOV1ADDR,
-  GOV2ADDR,
-  GOV3ADDR,
-  VALIDATORADDR,
-  USER1ADDR,
-  CHAINID,
-} from '@agoric/synthetic-chain/src/lib/constants.js';
+import { CHAINID } from '@agoric/synthetic-chain/src/lib/constants.js';
 
 import { agd } from '@agoric/synthetic-chain/src/lib/cliHelper.js';
-import { getUser } from '@agoric/synthetic-chain/src/lib/commonUpgradeHelpers.js';
-
-export const printKeys = async () => {
-  console.log('========== GOVERNANCE KEYS ==========');
-
-  console.log(`gov1: ${GOV1ADDR}`);
-  const gov1Key = await $`cat /root/.agoric/gov1.key`;
-  console.log(gov1Key.stdout);
-
-  console.log(`gov2: ${GOV2ADDR}`);
-  const gov2Key = await $`cat /root/.agoric/gov2.key`;
-  console.log(gov2Key.stdout);
-
-  console.log(`gov3: ${GOV3ADDR}`);
-  const gov3Key = await $`cat /root/.agoric/gov3.key`;
-  console.log(gov3Key.stdout);
-
-  console.log(`validator: ${VALIDATORADDR}`);
-  const validatorKey = await $`cat /root/.agoric/validator.key`;
-  console.log(validatorKey.stdout);
-
-  console.log(`user1: ${USER1ADDR}`);
-  const user1Key = await $`cat /root/.agoric/user1.key`;
-  console.log(user1Key.stdout);
-
-  const user2Address = await getUser('user2');
-
-  console.log(`user2: ${user2Address}`);
-  const user2Key = await $`cat /root/.agoric/user2.key`;
-  console.log(user2Key.stdout);
-};
 
 // submit a DeliverInbound transaction
 //
