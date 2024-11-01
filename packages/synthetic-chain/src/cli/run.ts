7afb14afa7a082df7e8d6863c5eff6a9afd5f557
Richard Gibson <richard.gibson@gmail.com>
2024-11-01 19:07:29 -0400

    feat(synthetic-chain): Propagate environment variable SLOGFILE into containers
    cf. https://docs.docker.com/reference/cli/docker/container/run/#env

--- packages/synthetic-chain/src/cli/run.ts
+++ packages/synthetic-chain/src/cli/run.ts
@@ -1,11 +1,29 @@
 import { execSync } from 'node:child_process';
+import { realpathSync } from 'node:fs';
 import { ProposalInfo, imageNameForProposal } from './proposals.js';
+
+/**
+ * Used to propagate a SLOGFILE environment variable into Docker containers.
+ * Any file identified by such a variable will be created if it does not already
+ * exist.
+ *
+ * @param {typeof process.env} env environment variables
+ * @returns {string[]} docker run options
+ */
+const propagateSlogfile = env => {
+  const { SLOGFILE } = env;
+  if (!SLOGFILE) return [];
+
+  execSync('touch "$SLOGFILE"');
+  return ['-e', 'SLOGFILE', '-v', `"$SLOGFILE:${realpathSync(SLOGFILE)}"`];
+};
 
 export const runTestImage = (proposal: ProposalInfo) => {
   console.log(`Running test image for proposal ${proposal.proposalName}`);
   const { name } = imageNameForProposal(proposal, 'test');
+  const slogOpts = propagateSlogfile(process.env);
   // 'rm' to remove the container when it exits
-  const cmd = `docker run --rm ${name}`;
+  const cmd = `docker run ${slogOpts.join(' ')} --rm ${name}`;
   execSync(cmd, { stdio: 'inherit' });
 };
 
@@ -28,7 +46,8 @@ export const debugTestImage = (proposal: ProposalInfo) => {
   `,
   );
 
+  const slogOpts = propagateSlogfile(process.env);
   // start the chain with ports mapped
-  const cmd = `docker run --publish 26657:26657 --publish 1317:1317 --publish 9090:9090 --interactive --tty --entrypoint /usr/src/upgrade-test-scripts/start_agd.sh ${name}`;
+  const cmd = `docker run ${slogOpts.join(' ')} --publish 26657:26657 --publish 1317:1317 --publish 9090:9090 --interactive --tty --entrypoint /usr/src/upgrade-test-scripts/start_agd.sh ${name}`;
   execSync(cmd, { stdio: 'inherit' });
 };
